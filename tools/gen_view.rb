fff7371e87a21571ac9c4e089470444bf5850891
wislylov <wislylou@firevale.com>
2016-09-06 00:06:08 +0800

    wip

--- tools/gen_view.rb
+++ tools/gen_view.rb
@@ -4,7 +4,7 @@ require_relative 'table_help'
 
 puts "check args:#{ARGV}"
 
-folder, view_name = ARGV
+folder, view_name, controller_name = ARGV
 
 VIEW_TEMEPLATE="
 <ion-view view-title=\"\" class=\"\">
@@ -17,15 +17,51 @@ save_str(VIEW_TEMEPLATE, "#{folder}/templates/wap/#{view_name}.html")
 
 
 # controllers_str="var g_controller = angular.module('starter.views', []);\n"
+controller_name = view_name
 
 views = read_to_hash("#{folder}/config/views.json")
 views ||= []
-views << view_name
+views << {"name" => view_name, "controller" => controller_name}
 
-# views.each do |one_controller|
-# 	controllers_str += "document.write(\"<script src='js/app/#{one_controller}.html'></script>\");\n"
-# end
-
-# save_str(controllers_str, "#{folder}/templates/app/views.js")
-# save_str(controllers_str, "#{folder}/templates/wap/views.js")
 save_to_json(views, "#{folder}/config/views.json")
+
+
+router_header_str = "
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+"
+
+router_str ={
+	"app" => router_header_str,
+	"wap" => router_header_str,
+}
+views.each do |one_view|
+	name = one_view["name"]
+	controller = one_view["controller"]
+	router_str.each do |key, value|
+		value +=
+		"  // Each tab has its own nav history stack:
+		
+		  .state('#{name}', {
+		    url: '/#{name}',
+		    views: {
+		      '#{name}': {
+		        templateUrl: 'templates/#{key}/#{name}.html',
+		        controller: '#{controller}Ctrl'
+		      }
+		    }
+		  }) \n"
+		  router_str[key] = value		
+	end
+end
+
+router_str.each do |key, value|
+	router_str[key] = value + "\n })"
+	save_str(router_str[key], "#{folder}/js/#{key}/router.js")
+end
--- tools/init_project.rb
+++ tools/init_project.rb
@@ -16,3 +16,41 @@ Dir.mkdir("#{folder}/config") unless  (File.exist?("#{folder}/config"))
 save_to_json([], "#{folder}/config/controllers.json")
 save_to_json([], "#{folder}/config/services.json")
 save_to_json([], "#{folder}/config/views.json")
+
+
+INIT_TEMPLATE = "
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+"
+
+save_str(INIT_TEMPLATE, "#{folder}/js/app/app.js")
+save_str(INIT_TEMPLATE, "#{folder}/js/wap/app.js")
+
+CONTROLLER_TEMPLATE = "
+var g_controller = angular.module('starter.controllers', []);
+"
+save_str(INIT_TEMPLATE, "#{folder}/js/app/controllers.js")
+save_str(INIT_TEMPLATE, "#{folder}/js/wap/controllers.js")
